8cc51069090f1800ab5f6e578976da109dcc191f
ezdegyadri-commits <ezdegyadri@gmail.com>
2026-09-13 17:56:10 -0600

    Create asignaciones.py

---
+++ services/services/asignaciones.py
@@ -0,0 +1,26 @@
+María José
+ ├─ Damián Carmona
+ ├─ Ichcaanziho
+ ├─ Elvira Parra Ávila
+ └─ Quintana Roo
+
+Abril
+ ├─ Gregorio Torres Quintero
+ ├─ Remigio Aguilar Sosa
+ ├─ Manuel Sarrado
+ └─ Domingo Solís Rodríguez
+
+Elmy
+ ├─ Damián Carmona
+ ├─ Ichcaanziho
+ ├─ Elvira Parra Ávila
+ └─ Quintana Roo
+
+Marilyn
+ ├─ Gregorio Torres Quintero
+ ├─ Remigio Aguilar Sosa
+ ├─ Manuel Sarrado
+ └─ Domingo Solís Rodríguez
+
+Diego
+ └─ TODAS
